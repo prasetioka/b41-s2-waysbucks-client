19b33d9b290f90a3cdc0753c9927e8be96e69817
prasetioka <prasetiokusumaatmaja@gmail.com>
2022-12-01 00:49:41 +0700

    Client Side

--- src/App.js
+++ src/App.js
@@ -7,22 +7,22 @@ import AddProductPage from './pages/AddProductPage'
 import AddTopingPage from './pages/AddTopingPage'
 import IncomePage from './pages/IncomePage'
 import Navbar from './components/Navbar'
-import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { Route, Routes} from 'react-router-dom'
 
 function App() {
   return (
-      <Router>
-        <Navbar/>
-        <Routes>
-            <Route exact path='/' element={<HomePage />} />
-            <Route exact path='/AddProductPage' element={<AddProductPage />} />
-            <Route exact path='/AddTopingPage' element={<AddTopingPage />} />
-            <Route exact path='/DetailProduct/:productid' element={<DetailProduct />} />
-            <Route exact path='/IncomePage' element={<IncomePage />} />
-            <Route exact path='/MyCart' element={<MyCart />} />
-            <Route exact path='/ProfilePage' element={<ProfilePage />} />
-        </Routes>
-      </Router>   
+        <>
+          <Navbar/>
+          <Routes>
+              <Route exact path='/' element={<HomePage />} />
+              <Route exact path='/AddProductPage' element={<AddProductPage />} />
+              <Route exact path='/AddTopingPage' element={<AddTopingPage />} />
+              <Route exact path='/DetailProduct/:productid' element={<DetailProduct />} />
+              <Route exact path='/IncomePage' element={<IncomePage />} />
+              <Route exact path='/MyCart' element={<MyCart />} />
+              <Route exact path='/ProfilePage' element={<ProfilePage />} />
+          </Routes>
+        </>
   )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,52 +1,115 @@
 import React, { useState } from 'react'
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
-import Form from 'react-bootstrap/Form';
-import Container from 'react-bootstrap/Container';
+import { Alert, Button, Modal, Form, Container } from 'react-bootstrap';
+import { UserContext } from '../context/userContext';
+import { useMutation } from 'react-query';
+import { API } from '../config/api';
 
 function Register({show, onHide, setLoginShow, setRegisterShow}) {
-        const user = []
-        const DataUser = localStorage.getItem("USER_DATA")
-        const UserData = JSON.parse(DataUser)
-        const [state, setState] = useState({
+        // const user = []
+        // const DataUser = localStorage.getItem("USER_DATA")
+        // const UserData = JSON.parse(DataUser)
+
+        const [message, setMessage] = useState(null);
+        const [form, setForm] = useState({
             fullname: "",
             email: "",
             password: "",
-            status: "user"
+            role: "user"
         })
 
-        const handleOnSubmit = (e) => {
-            e.preventDefault()
-            if (UserData == null) {
-                user.push(state)
-                localStorage.setItem("USER_DATA", JSON.stringify(user))
-            } else {
-                UserData.forEach((element) => {
-                    user.push(element)
-                })
-                user.push(state)
-                localStorage.setItem("USER_DATA", JSON.stringify(user))
+        const { fullname, email, password } = form;
+
+        const handleChange = (e) => {
+            setForm({
+              ...form,
+              [e.target.name]: e.target.value,
+            });
+          };
+
+        const handleSubmit = useMutation(async (e) => {
+            try {
+              e.preventDefault();
+          
+              // Configuration Content-type
+              const config = {
+                headers: {
+                  'Content-type': 'application/json',
+                },
+              };
+          
+              // Data body
+              const body = JSON.stringify(form);
+          
+              // Insert data user to database
+              const response = await API.post('/register', body, config);
+          
+              // Handling response here
+              if (response.data.status === 'success') {
+                // const alert = (
+                //   <Alert variant="success" className="py-1">
+                //     Success
+                //   </Alert>
+                // );
+                setMessage(alert);
+                setForm({
+                    fullname: "",
+                    email: "",
+                    password: "",
+                });
+                setLoginShow(true)
+                setRegisterShow(false)
+              } else {
+                const alert = (
+                  <Alert variant="danger" className="py-1">
+                    Failed
+                  </Alert>
+                );
+                setMessage(alert);
+              }
+            } catch (error) {
+              const alert = (
+                <Alert variant="danger" className="py-1">
+                  Failed
+                </Alert>
+              );
+              setMessage(alert);
+              console.log(error);
             }
-            setLoginShow(true)
-            setRegisterShow(false)
-        }
+          });
+
+        // const handleOnSubmit = (e) => {
+        //     e.preventDefault()
+        //     if (UserData == null) {
+        //         user.push(state)
+        //         localStorage.setItem("USER_DATA", JSON.stringify(user))
+        //     } else {
+        //         UserData.forEach((element) => {
+        //             user.push(element)
+        //         })
+        //         user.push(state)
+        //         localStorage.setItem("USER_DATA", JSON.stringify(user))
+        //     }
+        //     setLoginShow(true)
+        //     setRegisterShow(false)
+        // }
     
     return (
         <Modal show={show} onHide={onHide}  centered>
             <Container className="px-5 py-5">
-                <Form onSubmit={handleOnSubmit}>
+                {message && message}
+                <Form onSubmit={(e) => handleSubmit.mutate(e)}>
                     
                     <Form.Group controlId="formBasicEmail">
                         <Form.Label className="fs-1 mb-3 fw-bold" style={{color:'#bd0707'}}>Register</Form.Label>
-                        <Form.Control className="mb-3 py-3 fs-5" style={{borderColor:'#bd0707', borderWidth:'3px', backgroundColor:'rgb(224,200,200,0.25)'}} type="email" placeholder="Email" value={state.email} onChange={(e) => setState({...state, email: e.target.value})} />
+                        <Form.Control className="mb-3 py-3 fs-5" style={{borderColor:'#bd0707', borderWidth:'3px', backgroundColor:'rgb(224,200,200,0.25)'}} type="email" placeholder="Email" name="email" value={email} onChange={handleChange} />
                     </Form.Group>
 
                     <Form.Group className="mb-4" controlId="formBasicPassword">
-                        <Form.Control className="py-3 fs-5" style={{borderColor:'#bd0707', borderWidth:'3px', backgroundColor:'rgb(224,200,200,0.25)'}} type="password" placeholder="Password" value={state.password} onChange={(e) => setState({...state, password: e.target.value})} />
+                        <Form.Control className="py-3 fs-5" style={{borderColor:'#bd0707', borderWidth:'3px', backgroundColor:'rgb(224,200,200,0.25)'}} type="password" placeholder="Password" name="password" value={password} onChange={handleChange} />
                     </Form.Group>
 
                     <Form.Group className="mb-4" controlId="formBasicName">
-                        <Form.Control className="py-3 fs-5" style={{borderColor:'#bd0707', borderWidth:'3px', backgroundColor:'rgb(224,200,200,0.25)'}} type="text" placeholder="Full Name" value={state.fullname} onChange={(e) => setState({...state, fullname: e.target.value})} />
+                        <Form.Control className="py-3 fs-5" style={{borderColor:'#bd0707', borderWidth:'3px', backgroundColor:'rgb(224,200,200,0.25)'}} type="text" placeholder="Full Name" name="fullname" value={fullname} onChange={handleChange} />
                     </Form.Group>
 
                     <Button variant="primary" type="submit" style={{backgroundColor:'#bd0707', borderColor: '#bd0707'}} className="py-2 fw-bold fs-5 mb-4 w-100">
